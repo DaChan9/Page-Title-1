731f7bcba1a73098d2b7808373bfa5f0dec9b60d
DaChan9 <43589889+DaChan9@users.noreply.github.com>
2018-11-29 15:47:10 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#Houdini2").hide();
+$(".disappear").click(function(){
+$("#Houdini1").fadeOut(); 
+});
+
+$(".appear").click(function(){
+    $("#Houdini2").fadeIn();
+});
+$(".reapper").click(function(){
+    $("#Houdini3").toggle();
+});    
